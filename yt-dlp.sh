d53e6c5b1ac80c7fa849ce64d8c25aaa0a2cffb8
Mis1ify <111151942+Mis1ify@users.noreply.github.com>
2024-08-28 11:50:04 +0200

    Update yt-dlp.sh

--- yt-dlp.sh
+++ yt-dlp.sh
@@ -12,12 +12,12 @@ cd $directory
 # Download the video with English and Polish Subtitles
 #yt-dlp -f "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best" --write-sub --sub-lang en,pl --embed-metadata --parse-metadata "description:(?s)(?P<meta_comment>.+)" $1
 
-# Download with subs and put video miniature into it, and put videos from playlist to folder if a link is a playlist
+# Download 1. video; 2. subtitles; 3. video thumbnail; 4. put video thumbnail into video (embed); 5. put videos from playlist to folder if a link is a playlist
 
 if [[ $1 == *"playlist"* ]]; then
-    yt-dlp --embed-thumbnail -o "%(playlist_title)s/%(playlist_index)s - %(title)s.%(ext)s" -f "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best" --write-sub --sub-lang en,pl --embed-metadata --parse-metadata "description:(?s)(?P<meta_comment>.+)" "$1"
+    yt-dlp --embed-thumbnail --write-thumbnail -o "%(playlist_title)s/%(playlist_index)s - %(title)s.%(ext)s" -f "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best" --write-sub --sub-lang en,pl --embed-metadata --parse-metadata "description:(?s)(?P<meta_comment>.+)" "$1"
     
 else
 
-    yt-dlp --embed-thumbnail -f "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best" --write-sub --sub-lang en,pl --embed-metadata --parse-metadata "description:(?s)(?P<meta_comment>.+)" "$1"
+    yt-dlp --embed-thumbnail --write-thumbnail -f "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best" --write-sub --sub-lang en,pl --embed-metadata --parse-metadata "description:(?s)(?P<meta_comment>.+)" "$1"
 fi
